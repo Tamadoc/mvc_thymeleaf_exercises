3c120d4ccaf401a92f4f66e4d01155e52f41d337
Tamadoc <teri.sandstedt@gmail.com>
2021-03-31 21:26:50 +0200

    Initial commit: empty project with dependencies

---
+++ src/main/java/se/lexicon/teri/mvc_thymeleaf_exercises/MvcThymeleafExercisesApplication.java
@@ -0,0 +1,13 @@
+package se.lexicon.teri.mvc_thymeleaf_exercises;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MvcThymeleafExercisesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MvcThymeleafExercisesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/lexicon/teri/mvc_thymeleaf_exercises/MvcThymeleafExercisesApplicationTests.java
@@ -0,0 +1,13 @@
+package se.lexicon.teri.mvc_thymeleaf_exercises;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcThymeleafExercisesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
