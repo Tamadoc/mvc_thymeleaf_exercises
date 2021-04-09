a9acb8c1812391f8d7af1b3d5d9344b618f4fef6
Tamadoc <teri.sandstedt@gmail.com>
2021-04-09 16:33:59 +0200

    Exercise 2

--- src/main/java/se/lexicon/teri/mvc_thymeleaf_exercises/controller/HomeController.java
+++ src/main/java/se/lexicon/teri/mvc_thymeleaf_exercises/controller/HomeController.java
@@ -1,13 +1,48 @@
 package se.lexicon.teri.mvc_thymeleaf_exercises.controller;
 
+import com.sun.org.apache.bcel.internal.generic.NEW;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    List<String> contacts;
+
     @GetMapping("/")
     public String index() {
         return "index";
+    }
+
+    @GetMapping("/contact")
+    public String contact() {
+        return "contact";
+    }
+
+    @PostMapping("/contact")
+    public String contact(@RequestParam("new-contact") String newContact, Model model) {
+        if (contacts == null) {
+            contacts = new ArrayList<>();
+        }
+        contacts.add(newContact);
+
+        String confirmationMessage = "Contact added: " + newContact + ".";
+        model.addAttribute("confirmation", confirmationMessage);
+        return "contact-confirmation";
+    }
+
+    @GetMapping("/contact-list")
+    public String contactList(Model model) {
+        if (contacts == null) {
+            contacts = new ArrayList<>();
+        }
+        model.addAttribute("contacts", contacts);
+        return "show-contacts";
     }
 }
